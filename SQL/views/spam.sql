5f9cd10798c69551a52b1e08c99136cfbb8f2da3
Xu <weng.88@buckeyemail.osu.edu>
2018-03-26 20:33:55 -0400

    add 7296 in spam view

--- SQL/views/spam.sql
+++ SQL/views/spam.sql
@@ -2,5 +2,6 @@ create or replace view spam
   as select num, count(*) 
     from call_history 
     where name = 'SUPPORT INT' 
+      or num = '614-907-7296'
     group by num 
     order by count(*) desc;
